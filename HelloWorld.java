d2375b4dce5bff0d6c6c8537d333f06c5c2d309d
tom4860 <135623186+tom4860@users.noreply.github.com>
2023-06-22 19:46:50 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello Jenkins GIT World!");
+    System.out.println("Hello Jenkins I Am Thomas!");
   }
 }
